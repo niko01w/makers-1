bb8b959e0ad6a4b22fa784987af933fba8cfb683
niko01w <niko01w@mail.ru>
2022-10-03 17:03:12 +0600

    first commit

---
+++ test2/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+
+
+    def __str__(self):
+        return f'{self.owner} - {self.title}'
+
+class Comment(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
